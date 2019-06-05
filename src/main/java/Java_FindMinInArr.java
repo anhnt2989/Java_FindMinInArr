f1316ed4349aa6acd08965e228377a594a2a076d
anhnt2989 <anhnt2989@gmail.com>
2019-06-05 10:53:09 +0700

    nop bai

---
+++ src/main/java/Java_FindMinInArr.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Java_FindMinInArr {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size does not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.println("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+//        int []arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(array);
+        System.out.println("The smallest element in the array is: " + array[index]);
+    }
+    private static int minValue(int[] array){
+
+        int min = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++){
+            if(array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
